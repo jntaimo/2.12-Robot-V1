84a674f8cbef6fca7ad768a493035fde4fcead7b
jntaimo <jntaimo@gmail.com>
2022-12-25 17:58:52 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <PS4Controller.h>
+
+//Install https://github.com/aed3/PS4-esp32 for PS4 Remote
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
