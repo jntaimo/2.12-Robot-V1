ebe9bd17049c12745e2e7eb9c5f4b05497475ea1
jntaimo <jntaimo@gmail.com>
2022-12-25 18:26:17 -0500

    Successfully connected

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,22 @@
 #include <PS4Controller.h>
 
 //Install https://github.com/aed3/PS4-esp32 for PS4 Remote
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // Replace the "1a:2b:3c:01:01:01" with the MAC address
+  // the controller wants to pair to
+  // Note: MAC address must be unicast
+  PS4.begin("5c:f3:70:a6:b8:38");
+  Serial.println("Ready.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (PS4.isConnected()) {
+    Serial.println("Connected!");
+  }
+
+  delay(3000);
 }
